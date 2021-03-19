ea1647147a16bbec4d55c269ca8eaecd7f063cf9
FernandoMarcon <fmarcon13@gmail.com>
2021-03-19 18:02:55 -0300

    added random walker dir

---
+++ random_walker/random_walker.js
@@ -0,0 +1,35 @@
+var x;
+var y;
+
+function setup() {
+  createCanvas(400, 400);
+  x = 200;
+  y = 200;
+  background(51);
+}
+
+function draw() {
+  stroke(255);
+  strokeWeight(5);
+  point(x,y);
+  
+  var r = floor(random(4));
+  
+  switch(r) {
+      case 0:
+        x += 1;
+        break;
+      
+      case 1:
+        x = x - 1;
+        break;
+      
+      case 2:
+        y = y + 1;
+        break;
+
+      case 3:
+        y = y - 1;
+        break;
+         }
+}
