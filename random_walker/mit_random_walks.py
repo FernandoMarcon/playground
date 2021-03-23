086df3b06f93005451f59cd7e89cbcc5336cceda
FernandoMarcon <fmarcon13@gmail.com>
2021-03-23 15:20:46 -0300

    created script for random walker in python from MIT class

---
+++ random_walker/mit_random_walks.py
@@ -0,0 +1,12 @@
+class Location(object):
+    def __init__(self, x, y):
+        """x and y are floats"""
+        self.x = x
+        self.y = y
+    def move(self, deltaX, deltaY):
+        """deltaX and deltaY are floats"""
+        return Location(self.x + deltaX, self.y + deltaY)
+    def getX(self):
+        return self.x
+    def getY(self):
+        return self.y
